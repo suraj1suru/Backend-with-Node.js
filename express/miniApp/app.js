537512fca36f0aa1336e9c4db75bc2662c172d46
suraj patel <vikassuraj283@gmail.com>
2022-12-13 14:52:32 +0530

    :learning: mongoose, Schema, model, Validation.

--- express/miniApp/app.js
+++ express/miniApp/app.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const express = require("express");
+const mongoose = require("mongoose");
+
+const { db_link}  = require('./secrets')
 
 const app = express();
-
-app.listen(3000);
+app.use(express.json());
 const authRouter = express.Router();
 
 app.use("/auth", authRouter);
@@ -22,3 +24,47 @@ function postSignUp(req, res) {
     data: obj,
   });
 }
+
+app.listen(3000);
+
+
+//Db connection
+
+mongoose.connect(db_link)
+.then(function(db){
+console.log("db connected");
+})
+.catch(function (err){
+  console.log(err);
+  console.log("db not connected");
+});
+
+
+//Mongoose Schemaa
+
+const userSchema = mongoose.Schema({
+  email:{
+    type:String,
+    required:true,
+    unique:true
+  },
+  password :{
+    type:String,
+    required:true,
+    min:8
+
+  }
+})
+
+// model
+
+const userModel = mongoose.model('userModel', userSchema);
+
+(async function createUser(){
+  let user = {
+    email : 'abc@gmail.com',
+    password : 'ecwd46dcs'
+  };
+  let data = await userModel.create(user);
+  console.log(data);
+})();
